23f9a67c1903c9bc5764096fa982f9e547f8c7df
cesar0987 <cesarbenitez46@gmail.com>
2024-09-17 08:15:59 -0400

    Operadores

---
+++ src/OperadoresAritmeticos.java
@@ -0,0 +1,21 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        System.out.println("***Operadores Aritmeticos***");
+        double a =5, b=3, resultado=0;
+        //Suma +
+        resultado=a+b;
+        System.out.println("resultado suma= " + resultado);
+        //Resta -
+        resultado=a-b;
+        System.out.println("resultado  resta= " + resultado);
+        //Multiplicacion *
+        resultado=a*b;
+        System.out.println("resultado multiplicacion= " + resultado);
+        //Division /
+        resultado=a/b;
+        System.out.println("resultado division = " + resultado);
+        //Modulo(residuo division)
+        resultado=a%b;
+        System.out.println("resultado modulo= " + resultado);
+    }
+}
---
+++ src/OperadoresAsignacion.java
@@ -0,0 +1,7 @@
+public class OperadoresAsignacion {
+    public static void main(String[] args) {
+        System.out.println("***Operadores de Asignacion***");
+        //Asignacion
+        var miNumero=10;
+    }
+}
