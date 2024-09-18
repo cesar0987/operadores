e06ef7f00aac76bb0748e6aecb0523fedf6b9db1
cesar0987 <cesarbenitez46@gmail.com>
2024-09-18 08:40:17 -0400

    Operadores de Comparacion

--- src/OperadoresAsignacion.java
+++ src/OperadoresAsignacion.java
@@ -3,5 +3,17 @@ public class OperadoresAsignacion {
         System.out.println("***Operadores de Asignacion***");
         //Asignacion
         var miNumero=10;
+        int miNumero2;
+        miNumero2=15;
+        //Asignacion compuesta
+        //+=
+        miNumero +=5;//miNumero=miNumero+5;
+        System.out.println("miNumero = " + miNumero);
+        //-=,*=,/= y %=
+        miNumero *=2;//miNumero=miNumero*2
+        System.out.println("miNumero = " + miNumero);
+        //Asignacion de varialbes multiples
+        int a=10,b=15,c=20;
+        System.out.printf("a=%d,b=%d,c=%d",a,b,c);
     }
 }
