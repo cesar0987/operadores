d18496f983e18333ca2cb51941c826455249963a
cesar0987 <cesarbenitez46@gmail.com>
2024-10-05 07:35:58 -0400

    TickectVenta

--- src/TicketVenta.java
+++ src/TicketVenta.java
@@ -2,36 +2,49 @@ import java.util.Scanner;
 
 public class TicketVenta {
     public static void main(String[] args) {
-        System.out.println("***Generacion Ticket de Venta***");
+        System.out.print("***Generacion Ticket de Venta***");
         var consola = new Scanner(System.in);
 
-        System.out.println("Precio leche:");
+        System.out.print("Precio leche:");
         var precioLeche=Double.parseDouble(consola.nextLine());
 
-        System.out.println("Precio pan:");
+        System.out.print("Precio pan:");
         var precioPan = Double.parseDouble(consola.nextLine());
 
-        System.out.println("Precio lechuga:");
+        System.out.print("Precio lechuga:");
         var precioLechuga = Double.parseDouble(consola.nextLine());
 
-        System.out.println("Precio platanos");
+        System.out.print("Precio platanos");
         var precioPlatanos = Double.parseDouble(consola.nextLine());
+
+        System.out.print("Aplicar algun descuento(%)?");
+        var descuentoPorcentaje= Integer.parseInt(consola.nextLine());
+
 
         //Calculo del subtotal(sin impuestos)
         var subtotal = precioLeche+precioPan+precioLechuga+precioPlatanos;
 
+        //Aplicar el descuento
+        var descuento =subtotal *(descuentoPorcentaje/100.0);
+
+        //Subtotal con descuento
+        var subtotalConDescuento = subtotal -descuento;
+
         //Calculo con impuestos(16%)
-        var impuestos = subtotal*0.16;
+        var impuestos = subtotalConDescuento*0.16;
 
         //Calculo total de la compra (con impuestos)
-        var costoTotalCompra = subtotal + impuestos;
+        var costoTotalCompra = subtotalConDescuento + impuestos;
 
         //Imprimir el ticket de venta
         System.out.printf("""
+               %n Ticket de Venta
+               ---------------
                Subtotal: $%.2f
+               Descuento: $%.2f (%d%%)
                Impuesto(16%%):$%.2f
                Costo total de la compra: $%.2f 
-               """,subtotal,impuestos,costoTotalCompra);
+               """,subtotal,descuento,descuentoPorcentaje,impuestos,costoTotalCompra);
 
     }
 }
